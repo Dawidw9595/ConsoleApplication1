0c0796439267acdb5c951b766776e4c7e188a3a5
Dawidw9595 <dawidw9595@gmail.com>
2022-11-06 15:59:48 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   //Napisz program obliczania miejsc zerowych funkcji liniowej postaci ax + b gdzie a≠0 (wer.1.0) 
+	float a;
+	float b;
+	float x;
+	cout << " Oblilczanie miejsc zerowych funkcji ax+b" << endl << endl;
+	cout << "Podaj a gdzie a!=0" << endl;
+
+	cin >> a;
+	cout << "Podaj b" << endl;
+	cin >> b;
+	b = -b;
+	x = a / b;
+
+	cout << "Miejsce zerowe to " << x << endl;
+
+
+}
+
