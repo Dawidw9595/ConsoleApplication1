34aca76d477c15a3008b06808c305a16e5d1a864
Dawidw9595 <dawidw9595@gmail.com>
2023-01-10 10:09:24 +0100

    6.5

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,29 +2,158 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <locale.h>
 using namespace std;
+float a;
+float b;
+float x;
+float c;
+float delta = 0;
+float pierwiastek = 0;
+float x1 = 0;
+float x2 = 0;
+int i = 0;
+int przypadek = 0;
+void liniap1(float a, float b, float c)
+{
+	if (a == 0) {
+		cout << "Brak miejsc zerowych" << b;
+
+	}
+	else {
+		if (b == 0)
+		{
+			cout << "Rownanie sprzeczne dla tej funkcji" << endl;
+		}
+		else
+		{
+			b = -b;
+			x = a / b;
+			cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
+		}
+
+	}
+}
+void liniap2(float a, float b, float c)
+{
+	if (a == 0) {
+
+		cout << "Funkcja nie posiada miejsc zerowych " << endl;
+
+	}
+	else {
+		if (b == 0) {
+			b = -c;
+			x = a / b;
+			cout << "Miejsce zerowe funkcji ax + by + c to " << x << endl;
+		}
+		else {
+
+			if (c == 0) {
+				b = -b;
+				x = a / b;
+				cout << "Miejsce zerowe funkcji ax + bx + c to " << x << endl;
+			}
+			else {
+				b = -b;
+				a = a / b;
+				c = c / b;
+				c = -c;
+				x = a / c;
+				cout << "Miejsce zerowe funkcji ax + by + c to " << x << endl;
+			}
+		}
+
+	}
+}
+void kwadratp2(float a, float b, float c)
+{
+	if (a == 0) {
+		delta = b * b;
+		x1 = 0;
+		x2 = 0;
+		cout << "Delta wynosi: " << delta << endl << "x1 wynosi: " << x1 << endl << "x2 wyosi: " << x2 << endl;
+	}
+	else {
+		if (b == 0) {
+			delta = -4 * a * c;
+			if (delta < 0)
+			{
+				cout << "Delta wynosi: " << delta << endl << "To równanie kwadratowe nie ma rozwiązań" << endl;
+			}
+			else
+			{
+				if (delta == 0) {
+					x1 = -b / (2 * a);
+					cout << "delta wynosi: " << delta << "to równanie kwadratowe ma jedno rozwiązanie: " << x1 << endl;
+				}
+				else {
+					x1 = (-b + sqrt(delta)) / (2 * a);
+					x2 = (-b - sqrt(delta)) / (2 * a);
+					cout << "Delta wynosi: " << delta << endl << "x1 wynosi: " << x1 << endl << "x2 wyosi: " << x2 << endl;
+				}
+			}
+		}
+		else
+		{
+			if (c == 0) {
+				delta = b * b;
+				if (delta < 0)
+				{
+					cout << "Delta wynosi: " << delta << endl << " to równanie kwadratowe nie ma rozwiązań" << endl;
+				}
+				else
+				{
+					if (delta == 0) {
+						x1 = -b / (2 * a);
+						cout << "delta wynosi: " << delta << "to równanie kwadratowe ma jedno rozwiązanie: " << x1 << endl;
+					}
+					else {
+						x1 = (-b + sqrt(delta)) / (2 * a);
+						x2 = (-b - sqrt(delta)) / (2 * a);
+						cout << "Delta wynosi: " << delta << endl << "x1 wynosi: " << x1 << endl << "x2 wyosi: " << x2 << endl;
+					}
+				}
+			}
+			else {
+				delta = (b * b) - (4 * a * c);
+				if (delta < 0)
+				{
+					cout << "Delta wynosi: " << delta << endl << " to równanie kwadratowe nie ma rozwiązań" << endl;
+				}
+				else
+				{
+					if (delta == 0) {
+						x1 = -b / (2 * a);
+						cout << "delta wynosi: " << delta << "to równanie kwadratowe ma jedno rozwiązanie: " << x1 << endl;
+					}
+					else {
+						x1 = (-b + sqrt(delta)) / (2 * a);
+						x2 = (-b - sqrt(delta)) / (2 * a);
+						cout << "Delta wynosi: " << delta << endl << "x1 wynosi: " << x1 << endl << "x2 wyosi: " << x2 << endl;
+					}
+				}
+			}
+		}
+	}
+}
+
 int main()
 {
+	setlocale(LC_CTYPE, "Polish");
 	//Napisz program obliczania miejsc zerowych funkcji liniowej postaci ax + b gdzie a≠0 (wer.1.0) 
-	float a;
-	float b;
-	float negb;
-	float w;
-	float x;
-	float c;
-	float y;
-	float delta;
-	float x1;
-	float x2;
-	int przypadek;
 	
+
 	cout << "Program obliczania miejsc zerowych fyunkcji" << endl;
 	cout << "-------------------------------------------" << endl;
 	cout << "Wybierz funkcję do obliczenia" << endl;
 	cout << "1. ax+b" << endl;
 	cout << "2. Ax+By+C" << endl;
 	cout << "3. ax^2+bx+c" << endl;
+	cout << "4. Y=a(x-p)^2+q" << endl;
 	cout << "Chce obliczyć miejsce zerower funkcji: ";
+	while(przypadek==0){
 	cin >> przypadek;
 	switch (przypadek)
 	{
@@ -33,23 +162,7 @@ int main()
 		cin >> a;
 		cout << "Podaj b" << endl;
 		cin >> b;
-		if (a == 0) {
-			cout << "Brak miejsc zerowych" << b;
-
-		}
-		else {
-			if (b == 0)
-			{
-				cout << "Rownanie sprzeczne dla tej funkcji" << endl;
-			}
-			else
-			{
-				b = -b;
-				x = a / b;
-				cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
-			}
-			
-		}
+		liniap1(a, b, c);
 		break;
 	case 2:
 		cout << "Podaj a" << endl;
@@ -58,79 +171,63 @@ int main()
 		cin >> b;
 		cout << "podaj c" << endl;
 		cin >> c;
-		if (a == 0) {
-		
-		
-		}
-		else {
-			if (b == 0) {
-				cout << "rownanie sprzeczne dla ax+b " << endl;
-				negb = -c;
-				y = a / negb;
-				cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
-			}
-			else {
-
-				if (c == 0) {
-					negb = -b;
-					x = a / negb;
-					cout << "Miejsce zerowe funkcji ax^2 + bx + c to " << x << endl;
-					cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
-				}
-				else {
-					negb = -b;
-					x = a / negb;
-					cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
-
-					w = c / negb;
-					w = -w;
-					y = x / w;
-					cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
-		break;
+		liniap2(a, b, c);
+				break;
 	case 3:
+		cout << "Podaj a" << endl;
+		cin >> a;
+		cout << "Podaj b" << endl;
+		cin >> b;
+		cout << "podaj c" << endl;
+		cin >> c;
+		kwadratp2(a, b, c);
 		break;
 
 
 	default:
+		cout << "Nie wybrałeś, wybierz coś" << endl;
+		i++;
+
 		break;
-	}
+			
 
-
-/*
-	cout << " Oblilczanie miejsc zerowych funkcji ax+b, ax+by+c, ax^2+bx+c gdzie a=!0" << endl << endl;
-	
-	else {
-		if (b == 0) {
-			cout << "rownanie sprzeczne dla ax+b " << endl;
-			negb = -c;
-			y = a / negb;
-			cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
 		}
-		else {
+	}
+		/*
+			cout << " Oblilczanie miejsc zerowych funkcji ax+b, ax+by+c, ax^2+bx+c gdzie a=!0" << endl << endl;
 
-			if (c == 0) {
-				negb = -b;
-				x = a / negb;
-				cout << "Miejsce zerowe funkcji ax^2 + bx + c to " << x << endl;
-				cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
+			else {
+				if (b == 0) {
+					cout << "rownanie sprzeczne dla ax+b " << endl;
+					negb = -c;
+					y = a / negb;
+					cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
+				}
+				else {
+
+					if (c == 0) {
+						negb = -b;
+						x = a / negb;
+						cout << "Miejsce zerowe funkcji ax^2 + bx + c to " << x << endl;
+						cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
+					}
+					else{
+					negb = -b;
+					x = a / negb;
+					cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
+
+				w = c / negb;
+				w = -w;
+				y = x / w;
+				cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
+
+
+
+				cout << "Miejsce zerowe funkcji ax^2 + bx + c to " << y << endl;
+
 			}
-			else{
-			negb = -b;
-			x = a / negb;
-			cout << "Miejsce zerowe funkcji ax+b to " << x << endl;
-
-		w = c / negb;
-		w = -w;
-		y = x / w;
-		cout << "Miejsce zerowe funkcji ax + by + c to " << y << endl;
-
-
-
-		cout << "Miejsce zerowe funkcji ax^2 + bx + c to " << y << endl;
-
+			}
+			}
+			*/
 	}
-	}
-	}
-	*/
-}
 
